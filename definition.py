d221c8c0e1eb024b7160de8544b3601f58080095
Family <v.rostyk2001@gmail.com>
2023-02-27 13:52:10 +0200

    Create method to get definition from csv file

--- definition.py
+++ definition.py
@@ -1,6 +1,15 @@
+import pandas
+
+
 class Definition:
     def __init__(self, term):
         self.term = term
 
     def get(self):
-        pass
+        df = pandas.read_csv('data.csv')
+        definition = tuple(df.loc[df['word'] == self.term]['definition'])
+        return definition
+
+
+my_df = Definition('acid')
+print(my_df.get())
