6d25315b271171ccf4c374ade6eca1df9ac4c811
Family <v.rostyk2001@gmail.com>
2023-03-02 12:27:00 +0200

    Automatically get word definition

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from WebApp.About_Page import About
 from WebApp.Home_Page import Home
+from WebApp.DictionaryPage import Dictionary
 import justpy as jp
 
 jp.Route(Home.path, Home.serve)
 jp.Route(About.path, About.serve)
+jp.Route(Dictionary.path, Dictionary.serve)
 
 jp.justpy(port=5354)
 
